16debde31e20c9c1bf0919044bbb8f576622c9ce
jguachuns <jguachuns@unemi.edu.ec>
2023-04-10 14:35:56 -0500

    Integración de diseño css a todos los requeridos

--- pugstagram/src/components/TimeLine.svelte
+++ pugstagram/src/components/TimeLine.svelte
@@ -1,6 +1,12 @@
 <script>
     import Card from './Card.svelte'
 </script>
+<!-- TimeLine.svelte -->
+<style>
+    .TimeLine {
+      padding: 4em 0 0 0;
+    }
+</style>
 <div class="TimeLine">
     <div class="TimeLine-container">
         <Card />
